344c399a6454d5c7d61a8c146b79691e73542b7e
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-02-13 01:58:06 +0600

    Final Calcation doen

--- Shopping Cart/app.js
+++ Shopping Cart/app.js
@@ -11,14 +11,36 @@ function updateProdustNumber(product, price, isIncreasing) {
     // update  total
     const productTotal = document.getElementById(product + "-total");
     productTotal.innerText = caseNumber * price;
+
+    // calculate total
+    calculateTotal();
+}
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + "-number");
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+function calculateTotal() {
+    const phoneTotal = getInputValue("phone") * 1219;
+    const caseTotal = getInputValue("case") * 59;
+    const subTotal = phoneTotal + caseTotal;
+
+    const tax = subTotal * 0.1;
+    const grandTotal = subTotal + tax;
+
+    document.getElementById("sub-total").innerText = subTotal;
+    document.getElementById("tax-amount").innerText = tax.toFixed(2);
+    document.getElementById("total-amount").innerText = grandTotal.toFixed(2);
 }
 // phone increase descrease
 document.getElementById("phone-plus").addEventListener("click", function() {
-    updateProdustNumber("phone", 1259, true);
+    updateProdustNumber("phone", 1219, true);
 });
 
 document.getElementById("phone-minus").addEventListener("click", function() {
-    updateProdustNumber("phone", 1259, false);
+    updateProdustNumber("phone", 1219, false);
 });
 
 // handle increase decrease
