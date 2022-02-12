fa59dea0c99c9b4217360b4233c5d9a3c5f07d21
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-02-13 00:51:44 +0600

    case section added

--- Shopping Cart/app.js
+++ Shopping Cart/app.js
@@ -1,7 +1,16 @@
 function updateCaseNumber(isIncreasing) {
     const caseInput = document.getElementById("case-number");
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
+    let caseNumber = caseInput.value;
+    if (isIncreasing == true) {
+        caseNumber = parseInt(caseNumber) + 1;
+    } else if (caseInput.value > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+
+    // update case total
+    const caseTotal = document.getElementById("case-total");
+    caseTotal.innerText = caseNumber * 59;
 }
 
 document.getElementById("case-plus").addEventListener("click", function() {
@@ -9,7 +18,5 @@ document.getElementById("case-plus").addEventListener("click", function() {
 });
 
 document.getElementById("case-minus").addEventListener("click", function() {
-    const caseInput = document.getElementById("case-number");
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) - 1;
+    updateCaseNumber(false);
 });
